c5739192544369594ba936827afa1fb266bbba1a
Salim Karaoğlu <96123185+SalimKaraoglu@users.noreply.github.com>
2023-04-18 23:29:42 +0300

    Create Counter_Multiplication.sol

---
+++ Counter_Multiplication.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.6.6;
+
+contract gfgMultiply {
+
+	int128 firstNo ;
+	int128 secondNo ;
+	
+	function firstNoSet(int128 x) public {
+		firstNo = x;
+	}
+	
+	function secondNoSet(int128 y) public {
+		secondNo = y;
+	}
+	
+	function multiply() view public returns (int128) {
+		int128 answer = firstNo * secondNo ;
+		return answer;
+	}
+
+}
